4eefb0d4c353f0aa8b4747274d15f41b27555496
Phil-Gates <mateopw10@penguin>
2021-12-18 09:05:07 -0700

    '#' cmd

--- brainstack.py
+++ brainstack.py
@@ -1,6 +1,7 @@
 import sys
 import argparse
 import logging
+from typing import Any
 
 logger = logging.getLogger(__name__)
 
@@ -32,44 +33,82 @@ def _parse_args():
 
 
 class Stack:
-    def __init__(self):
+    """Storage for the program."""
+
+    def __init__(self) -> None:
+        """
+        Initialize variables.
+        self.items: list version of stack
+        """
         self.items = []
 
-    def push(self, item):
+    def push(self, item: Any) -> None:
+        """Push an item to the stack."""
         try:
             self.items.append(item.replace("&gt;", ">"))
         except AttributeError:
             self.items.append(item)
 
-    def pop(self):
+    def pop(self) -> Any:
+        """Take an item from the stack and return it."""
         try:
             return self.items.pop()
         except IndexError:
-            pass
+            logger.exception("cannot pop from empty list")
 
-    def copy(self):
+    def copy(self) -> None:
+        """Copy the current top of stack on top of itself."""
         top_val = self.pop()
         self.push(top_val)
         self.push(top_val)
 
-    def flip(self):
+    def flip(self) -> None:
+        """Flip the stack."""
         self.items = self.items[::-1]
 
-    def move(self, index):
-        self.push(self.items.pop(index))
+    def move(self, index: int) -> None:
+        """Move index specified to top of stack. (index starts from bottom [0])"""
+        try:
+            self.push(self.items.pop(index))
+        except ValueError:
+            logger.exception(f"invalid index for move: {index}")
 
-    def operation(self, _type):
-        if _type in ["+", "-", "*", "/"]:
-            self.push(eval(f"{self.pop()}{_type}{self.pop()}"))
-        else:
-            logger.exception(f"invalid operation: {_type}")
+    def toascii(self) -> None:
+        """Converts top value of stack to a character."""
+        try:
+            using = self.pop()
+            self.push(chr(using))
+        except TypeError:
+            logger.exception(f"invalid type for converting to char: {using}")
+
+    def operation(self, _type: str) -> None:
+        """Preform an operation on first two items on stack. (+, -, *, /)"""
+        try:
+            if _type in ["+", "-", "*", "/"]:
+                first = self.pop()
+                second = self.pop()
+                if isinstance(first, str) and isinstance(second, str):
+                    self.push(eval(f"'{first}'{_type}'{second}'"))
+                else:
+                    self.push(eval(f"{first}{_type}{second}"))
+            else:
+                logger.exception(f"invalid operation: {_type}")
+        except SyntaxError:
+            logger.exception(f"cannot preform operation '{_type}'")
 
 
 class Compiler:
-    def __init__(self, dump) -> None:
+    """Store dump for compilation and compile."""
+
+    def __init__(self, dump: str) -> None:
+        """
+        Get dump loc..
+        self.dump: dump loc.
+        """
         self.dump = dump
 
-    def compile(self, script):
+    def compile(self, script: str) -> None:
+        """Compile a brainstack script to the dump."""
         tabs = ""
         push_mode = False
         op_mode = False
@@ -79,20 +118,19 @@ class Compiler:
             file.write("S = Stack()\n")
             for cmd in script:
                 if (not push_mode) and (not op_mode) and (not mv_mode):
-                    if cmd == "&":
+                    if cmd == "<":
                         file.write(tabs + "S.push(")
-                    elif cmd == "<":
                         push_mode = True
-                    elif cmd == "#":
-                        file.write(tabs + "S.move(")
                     elif cmd == "{":
+                        file.write(tabs + "S.move(")
                         mv_mode = True
-                    elif cmd == "!":
-                        file.write(tabs + "while S.pop() != 0:\n")
                     elif cmd == "[":
+                        file.write(tabs + "while S.pop() != 0:\n")
                         tabs += "\t"
                     elif cmd == "]":
                         tabs = tabs[:-1]
+                    elif cmd == "#":
+                        file.write(tabs + "S.toascii()\n")
                     elif cmd == "$":
                         file.write(tabs + "S.operation(")
                         op_mode = True
@@ -118,7 +156,7 @@ class Compiler:
                     else:
                         file.write(cmd)
                 elif op_mode:
-                    file.write('"' + cmd + '"')
+                    file.write("'" + cmd + "'")
                     file.write(")\n")
                     op_mode = False
                 elif mv_mode:
@@ -128,7 +166,8 @@ class Compiler:
                     else:
                         file.write(cmd)
 
-    def compile_file(self, file_name):
+    def compile_file(self, file_name: str) -> None:
+        """Compile a file."""
         with open(file_name, "r") as file:
             script = file.read()
         self.compile(script)
